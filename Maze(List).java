789c8ab8761a65e5504e8522f9dd64af99cfadcb
w-zx <holywrong@gmail.com>
2016-12-26 22:56:10 +0800

    update

--- Maze(List).java
+++ Maze(List).java
@@ -1,21 +1,35 @@
-import java.io.*;
 import java.util.*;
 
+/**
+ * Created by arthurwang on 16/12/26.
+ */
 class Maze {
-    private int height = 10;
-    private int width = 10;
+    /**
+     * set the default value
+     */
+    private int height = 15;
+    private int width = 15;
     private int N = 1;
     private int S = 2;
     private int E = 4;
     private int W = 8;
-    private Map DX = new HashMap();
-    private Map DY = new HashMap();
-    private Map OPPOSITE = new HashMap();
-    private List < Integer > directions = Arrays.asList(N, S, E, W);
-    private List < List < Integer >> grid = new ArrayList < List < Integer >> ();
-    long seed = System.nanoTime();
 
-    public void mapping() {
+    private List< Integer > directions;
+    private List < List < Integer >> grid;
+    private HashMap DX;
+    private HashMap DY;
+    private HashMap OPPOSITE;
+
+    public Maze() {
+        directions = Arrays.asList(N, S, E, W);
+        grid = new ArrayList<>();
+        DX = new HashMap();
+        DY = new HashMap();
+        OPPOSITE = new HashMap();
+        mapping(DX, DY, OPPOSITE);
+    }
+
+    public void mapping(HashMap DX, HashMap DY, HashMap OPPOSITE) {
         DX.put(E, 1);
         DX.put(W, -1);
         DX.put(N, 0);
@@ -32,22 +46,77 @@ class Maze {
         OPPOSITE.put(S, N);
     }
 
+    /**
+     * give that if a number is between (min, max) range
+     * @param i
+     * @param minValueInclusive
+     * @param maxValueInclusive
+     * @return
+     */
     public boolean between(int i, int minValueInclusive, int maxValueInclusive) {
-        if (i >= minValueInclusive && i <= maxValueInclusive)
-            return true;
-        else
-            return false;
+        return (i >= minValueInclusive && i <= maxValueInclusive);
     }
 
+    /**
+     * initialize the number grid with 0
+     * @param width
+     * @param height
+     */
+    public void initGrid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        for (int i = 0; i < height; i++) {
+            List < Integer > list = new ArrayList <> (Collections.nCopies(width, 0));
+            grid.add(list);
+        }
+    }
+
+    /**
+     * generate the path
+     * @param cx
+     * @param cy
+     * @param grid
+     */
+    public void carve_passage_from(int cx, int cy, List < List < Integer >> grid) {
+        Collections.shuffle(directions, new Random(System.nanoTime()));
+
+        for (Integer direction: directions) {
+            int nx = cx + (int) DX.get(direction);
+            int ny = cy + (int) DY.get(direction);
+
+            /*
+            System.out.println("dir= " + direction +
+            "("+Integer.toBinaryString(direction)+")"+
+            ", cx= "+cx+", cy= "+cy+", nx= " + nx + ", ny= "+ ny);
+            */
+
+            if (between(ny, 0, grid.size() - 1) && between(nx, 0, grid.get(ny).size() - 1) && (grid.get(ny).get(nx) == 0)) {
+                grid.get(cy).set(cx, (grid.get(cy).get(cx) | direction));
+                grid.get(ny).set(nx, (grid.get(ny).get(nx) | (int) OPPOSITE.get(direction)));
+                // printArray(grid);
+                // System.out.println("______________");
+                carve_passage_from(nx, ny, grid);
+            }
+        }
+    }
+
+    /**
+     * print the number grid
+     * @param grid
+     */
     public void printArray(List < List < Integer >> grid) {
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid.get(i).size(); j++) {
-                System.out.print(grid.get(i).get(j) + " ");
+        for(List<Integer> ele : grid) {
+            for (Integer i : ele) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
     }
 
+    /**
+     * print the maze
+     * @param grid
+     */
     public void printMaze(List < List < Integer >> grid) {
         System.out.println(" " + new String(new char[width * 2 - 1]).replace("\0", "_"));
         for (int y = 0; y < height; y++) {
@@ -64,44 +133,15 @@ class Maze {
         }
     }
 
-
-    public void carve_passage_from(int cx, int cy, List < List < Integer >> grid) {
-        Collections.shuffle(directions, new Random());
-
-        for (Integer direction: directions) {
-            int nx = cx + (int) DX.get(direction);
-            int ny = cy + (int) DY.get(direction);
-
-            /*
-            System.out.println("dir= " + direction +
-            "("+Integer.toBinaryString(direction)+")"+
-            ", cx= "+cx+", cy= "+cy+", nx= " + nx + ", ny= "+ ny);
-            */
-
-            if (between(ny, 0, grid.size() - 1) && between(nx, 0, grid.get(ny).size() - 1) && (grid.get(ny).get(nx) == 0)) {
-                grid.get(cy).set(cx, (grid.get(cy).get(cx) | direction));
-                grid.get(ny).set(nx, (grid.get(ny).get(nx) | new Integer((int) OPPOSITE.get(direction))));
-                // printArray(grid);
-                // System.out.println("______________");
-                carve_passage_from(nx, ny, grid);
-            }
-        }
-    }
-
-    public void initGrid(int width, int height) {
-        this.width = width;
-        this.height = height;
+    public void generateMaze(String[] args) {
+        initGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        carve_passage_from(0, 0, grid);
+//        printArray(grid);
+        printMaze(grid);
     }
 
     public static void main(String[] args) throws java.lang.Exception {
         Maze maze = new Maze();
-        // maze.initGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        maze.mapping();
-        for (int i = 0; i < maze.width; i++) {
-            List < Integer > list = new ArrayList < Integer > (Collections.nCopies(maze.width, 0));
-            maze.grid.add(list);
-        }
-        maze.carve_passage_from(0, 0, maze.grid);
-        maze.printMaze(maze.grid);
+        maze.generateMaze(args);
     }
 }
